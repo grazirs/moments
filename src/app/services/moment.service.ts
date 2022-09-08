971418111c2e5805f4161dcb57078d86f9ed9965
grazirs <graziribeiro95@gmail.com>
2022-09-08 15:23:37 +0200

    update moment service and add get moments to home page

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,8 +1,9 @@
-import { createNgModule, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Moment } from '../interfaces/Moment';
 import { environment } from 'src/environments/environment';
+import { Response } from '../interfaces/Response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class MomentService {
   private apiUrl =`${this.baseApiUrl}/api/moments`
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
