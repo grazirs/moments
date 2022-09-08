60c8de2e4de82603a6f687ad39e5698f9fed5190
grazirs <graziribeiro95@gmail.com>
2022-09-08 17:44:41 +0200

    add data to home page from server

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -7,29 +7,29 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  allMoments: Moment[] = []
-  moments: Moment[] = []
-  baseApiUrl = environment.baseApiUrl
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
   //todo search
 
-  constructor(private momentService: MomentService ) { }
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
     this.momentService.getMoments().subscribe((items) => {
-      const data = items.data
+      const data = items.data;
 
       data.map((item) => {
-        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
-      })
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
 
       this.allMoments = data;
       this.moments = data;
-    })
+    });
   }
-
 }
