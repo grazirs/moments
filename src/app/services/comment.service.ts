b5f3860cdbb4ca46e12971c874db082b9ffb732b
grazirs <graziribeiro95@gmail.com>
2022-09-20 11:47:36 -0300

    update comment service

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Comment } from '../interfaces/Comment';
+import { Response } from '../interfaces/Response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}/api/moments`;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createComment(data: Comment): Observable<Response<Comment>> {
+    const url = `${this.apiUrl}/${data.momentId}/comments`;
+    return this.http.post<Response<Comment>>(url, data);
+  }
 }
